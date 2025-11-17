372a0e0f6f824bcae1bad5759e8b587a2d2ea331
Artem Fedoseev <Artikul563@gmail.com>
2025-11-17 19:26:01 +0300

    прописал логику для contact и доделал about us

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,26 @@
-from flask import render_template, request,redirect,url_for
+from flask import render_template, request, redirect, url_for,flash
 
 from app import app
+
 
 @app.route("/")
 def main():
     return render_template("index.html")
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/contact")
+def contact(methods=["GET","POST"]):
+    if request.method == "Get":
+        return redirect(url_for("index"))
+    else:
+        name=request.form.get("name")
+        email=request.form.get("email")
+        surname=request.form.get("surname")
+        age=request.form.get("age")
+        flash('Данные успешно отправлены')
+        return redirect(url_for("contact"))
